4bbfafb9c0cb7aa3c66bcbb3e4382024e4f425ed
Andrew Campbell <andrew.campbell@paconsulting.com>
2020-01-29 11:17:25 +0000

    Modified export function to use module in parent projects

--- src/index.js
+++ src/index.js
@@ -14,3 +14,5 @@ ReactDOM.render(el, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export default (e) => <TableauComponent {...e} />
