ecc9ad1f2d83502ddd361833dc82a93065f1f3f3
Andrew Campbell <andrew.campbell@paconsulting.com>
2020-02-10 15:39:48 +0000

    Updating merge.bat to mitigate merge issues with master branch & Realigning version number

--- merge.bat
+++ merge.bat
@@ -8,14 +8,14 @@ git add .
 ECHO %sourceBranch% COMMIT
 git commit -am "CI Build Merge to Master [skip ci]"
 
-ECHO CHECKOUT master
-git checkout master
-ECHO PULL master
-git pull origin master
-ECHO master STATUS
-git status
-ECHO MERGE %sourceBranch% into master
-git merge %sourceBranch% -m "CI Build Merge to Master [skip ci]"
+REM ECHO CHECKOUT master
+REM git checkout master
+REM ECHO PULL master
+REM git pull origin master
+REM ECHO master STATUS
+REM git status
+REM ECHO MERGE %sourceBranch% into master
+REM git merge %sourceBranch% -m "CI Build Merge to Master [skip ci]"
 
 ECHO master STATUS
 git status
