0ba43f5018f510e92e46b3d5b0ef372437b8f6b0
Andrew Campbell <andrew.campbell@paconsulting.com>
2020-02-10 15:33:13 +0000

    Updating merge.bat to mitigate merge issues with master branch & Realigning version number

--- merge.bat
+++ merge.bat
@@ -1,19 +1,27 @@
 @echo off
 ECHO SOURCE BRANCH IS %BUILD_SOURCEBRANCH%
-IF %BUILD_SOURCEBRANCH% == refs/heads/master (
-   ECHO Building master branch so no merge is needed.
-   EXIT
-)
 SET sourceBranch=origin/%BUILD_SOURCEBRANCH:refs/heads/=%
-ECHO GIT CHECKOUT MASTER
+ECHO %sourceBranch% STATUS
+git status
+ECHO %sourceBranch% ADD
+git add .
+ECHO %sourceBranch% COMMIT
+git commit -am "CI Build Merge to Master [skip ci]"
+
+ECHO CHECKOUT master
 git checkout master
-ECHO GIT STATUS
+ECHO PULL master
+git pull origin master
+ECHO master STATUS
 git status
-ECHO GIT MERGE
-git merge %sourceBranch% -m "Merge to master"
-ECHO GIT STATUS
+ECHO MERGE %sourceBranch% into master
+git merge %sourceBranch% -m "CI Build Merge to Master [skip ci]"
+
+ECHO master STATUS
 git status
-ECHO GIT PUSH
+ECHO master ADD
+git add .
+ECHO master COMMIT
+git commit -am "CI Build Merge to Master [skip ci]"
+ECHO master PUSH
 git push origin
-ECHO GIT STATUS
-git status
